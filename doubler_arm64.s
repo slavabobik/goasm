22afe7b150ca3e8572fa48094914440460fc061d
slavabobik <bobik.slava@yandex.ru>
2022-12-26 19:05:30 +0200

    Initial commit

---
+++ doubler_arm64.s
@@ -0,0 +1,6 @@
+// func Doubler(x int64) int64
+TEXT ·Doubler(SB),$0
+	MOVD	x+0(FP), R0
+    ADD     R0, R0
+	MOVD	R0, ret+8(FP)
+	RET
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+)
+
+func Doubler(int64) int64
+
+func main() {
+	fmt.Println(Doubler(6))
+}
